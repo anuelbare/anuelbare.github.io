9fda52a093daa0b17b8364fdee57edba12c0e6a7
anuelbare <gobledin398@gmail.com>
2024-08-16 15:11:20 +0100

    Add files via upload

---
+++ whoPaysTheBill.py
@@ -0,0 +1,18 @@
+import random
+# friends = ["Alice", "Bob", "Charlie", "David", "Emmanuel"]
+# payment = random.randint(0, 4)
+# if payment == 0:
+#     print("Alice")
+# elif payment == 1:
+#     print("Bob")
+# elif payment == 2:
+#     print("Charlie")
+# elif payment == 3:
+#     print("David")
+# else:
+#     print("Emmanuel")
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+payment = random.choice(friends)
+
+print(payment)
