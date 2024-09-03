009fe739270ff85ff4982535de0d421822844c5a
anuelbare <gobledin398@gmail.com>
2024-09-03 18:25:29 +0100

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,36 @@
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
+
+# TODO-1: - Use a while loop to let the user guess again.
+game_over = False
+
+red = []
+while not game_over:
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+
+    # TODO-2: Change the for loop so that you keep the previous correct letters in display.
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            red.append(guess)
+        elif letter in red:
+            display += letter
+        else:
+            display += "_"
+
+    print(display)
+
+    if "_" not in display:
+        print("You win")
