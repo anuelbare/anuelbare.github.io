4db33ecb9eeaacfc58eff7f105426751f074291b
anuelbare <gobledin398@gmail.com>
2024-09-13 23:21:45 +0100

    Update

---
+++ alarm_clock.py
@@ -0,0 +1,12 @@
+import time
+import os
+
+def alarm_clock(seconds):
+    print(f"Alarm set for {seconds} seconds.")
+    time.sleep(seconds)
+    print("Time's up! Wake up!")
+    # Play a sound (requires external sound file or os-level beep command)
+    os.system("echo -e '\a'")
+
+seconds = int(input("Enter time in seconds to set the alarm: "))
+alarm_clock(seconds)
